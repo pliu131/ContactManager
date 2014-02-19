d2efc27fb66e47d61d046d0f2ffb7f46a8414a92
Peter Liu <pliu7@binghamton.edu>
2014-02-19 17:36:52 -0500

    implement validations on person

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 describe Person do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid' do
+    person = Person.new(first_name: "John", last_name: "Doe")
+    expect(person).to be_valid
+  end
+  it 'is valid without a first name' do
+    person = Person.new(first_name: nil)
+    expect(person).not_to be_valid
+  end
+  it 'is valid without a last name' do
+    person = Person.new(last_name: nil)
+    expect(person).not_to be_valid
+  end
 end
