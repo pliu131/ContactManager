34eb229074c6fa69891c107c76834f8e6fc2e616
Peter Liu <pliu7@binghamton.edu>
2014-02-19 17:17:56 -0500

    added unicorn and rspec gems

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ContactManager::Application.config.secret_key_base = '5ca6e9d0a10da373c3768e00b98a48bf2763940941f87ef539d34b5647de4775521f2c2f690ee5b1959f08be5ca8a31df04773976810dee148d6d3a6ad110b3f'
