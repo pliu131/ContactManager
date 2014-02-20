93ee89f3d477615a5352110e12bef6a15d752345
Peter Liu <pliu7@binghamton.edu>
2014-02-19 21:23:06 -0500

    can create new email addresses

--- spec/features/person_view_spec.rb
+++ spec/features/person_view_spec.rb
@@ -3,48 +3,68 @@ require 'capybara/rails'
 require 'capybara/rspec'
 
 describe 'the person view', type: :feature do
+  let(:person) { Person.create(first_name: 'John', last_name: 'Doe') }
 
-  let(:person) { Person.create(first_name: 'John', last_name: 'Doe')}
+  context 'phone numbers' do
 
-  before(:each) do
-    person.phone_numbers.create(number: '555-1234')
-    person.phone_numbers.create(number: '555-5678')
-    visit person_path(person)
-  end
+    before(:each) do
+      person.phone_numbers.create(number: '555-1234')
+      person.phone_numbers.create(number: '555-5678')
+      visit person_path(person)
+    end
 
-  it 'shows the phone numbers' do
-    person.phone_numbers.each do |phone|
-      expect(page).to have_content(phone.number)
+    it 'shows the phone numbers' do
+      person.phone_numbers.each do |phone|
+        expect(page).to have_content(phone.number)
+      end
+    end
+
+    it 'has a link to add a new phone number' do
+      expect(page).to have_link('Add phone number', href: new_phone_number_path(person_id: person.id))
+    end
+
+    it 'adds a new phone number' do
+      page.click_link('Add phone number')
+      page.fill_in('Number', with: '555-8888')
+      page.click_button('Create Phone number')
+      expect(current_path).to eq(person_path(person))
+      expect(page).to have_content('555-8888')
+    end
+
+    it 'has links to edit phone numbers' do
+      person.phone_numbers.each do |phone|
+        expect(page).to have_link('edit', href: edit_phone_number_path(phone))
+      end
+    end
+
+    it 'edits a phone number' do
+      phone = person.phone_numbers.first
+      old_number = phone.number
+
+      first(:link, 'edit').click
+      page.fill_in('Number', with: '555-9191')
+      page.click_button('Update Phone number')
+      expect(current_path).to eq(person_path(person))
+      expect(page).to have_content('555-9191')
+      expect(page).not_to have_content(old_number)
     end
   end
 
-  it 'has a link to add a new phone number' do
-    expect(page).to have_link('Add phone number', href: new_phone_number_path(person_id: person.id))
-  end
-
-  it 'adds a new phone number' do
-    page.click_link('Add phone number')
-    page.fill_in('Number', with: '555-8888')
-    page.click_button('Create Phone number')
-    expect(current_path).to eq(person_path(person))
-    expect(page).to have_content('555-8888')
-  end
-
-  it 'has links to edit phone numbers' do
-    person.phone_numbers.each do |phone|
-      expect(page).to have_link('edit', href: edit_phone_number_path(phone))
+  context 'email addresses' do
+    before(:each) do
+      person.email_addresses.create(address: 'user1@example.com', person_id: 1)
+      person.email_addresses.create(address: 'user2@example.com', person_id: 1)
+      visit person_path(person)
     end
-  end
 
-  it 'edits a phone number' do
-    phone = person.phone_numbers.first
-    old_number = phone.number
+    it 'shows the email addresses' do
+      person.email_addresses.each do |email|
+        expect(page).to have_selector('li', text: email.address)
+      end
+    end
 
-    first(:link, 'edit').click
-    page.fill_in('Number', with: '555-9191')
-    page.click_button('Update Phone number')
-    expect(current_path).to eq(person_path(person))
-    expect(page).to have_content('555-9191')
-    expect(page).not_to have_content(old_number)
+    it 'has a link to add a new email address' do
+      expect(page).to have_link('Add email address', href: new_email_address_path(person_id: person.id))
+    end
   end
 end
